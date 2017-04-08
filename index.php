25a3ab3d98b85594262d9a0dd28724642d4eef3c
tutorialcid <adaniblog852@gmail.com>
2017-04-07 20:53:24 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @tutorialcid
+ Sign out
+ Watch 1
+  Star 1
+  Fork 3 willyarisky/safelink
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs
+Branch: master Find file Copy pathsafelink/index.php
+164d94e  on Dec 10, 2014
+@willyarisky willyarisky safelink
+1 contributor
+RawBlameHistory     
+117 lines (100 sloc)  4.93 KB
 <?php
 // Willy Arisky
 require('config.php');
@@ -14,7 +32,7 @@ require('config.php');
     <title><?php echo $title; ?></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="assets/ccs/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="assets/navbar.css" rel="stylesheet">
@@ -31,17 +49,6 @@ require('config.php');
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-        <!-- BEGIN ADREACTOR CODE -->
-    <script data-cfasync="false">
-    var _avp = _avp || [];
-    (function() {
-    var s = document.createElement('script');
-    s.type = 'text/javascript'; s.async = true; s.src = window.location.protocol + '//adserver.adreactor.com/js/libcode3.js';
-    var x = document.getElementsByTagName('script')[0];
-    x.parentNode.insertBefore(s, x);
-    })();
-    </script>
-    <!-- END ADREACTOR CODE -->
   </head>
 
   <body>
@@ -63,8 +70,8 @@ require('config.php');
           <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
               <li class="active"><a href="/"><?php echo $title; ?></a></li>
-              <li><a rel="dofollow" href="http://www.marwanto606.xyz">Marwanto606</a></li>
-              <li><a rel="dofollow" href="http://www.learnmore-seemore.com">Streaming Video</a></li>
+              <li><a href="http://citizen.co.id">News</a></li>
+              <li><a href="http://tv.citizen.co.id">TV Online</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
@@ -76,73 +83,41 @@ require('config.php');
 Link Decrypter is a service that is used to protect a link.
 With Link Decrypter, you also protect your blog / website belongs to you because it uses a null referer, so it can not be tracked.
 Link Decrypter is free, so we put some ads to make this website alive.
-        <center>    <!-- BEGIN ADREACTOR CODE -->
-    <div id="avp_zid_9">
-    <script>
-    _avp.push({ tagid: 'avp_zid_9', alias: '/', type: 'banner', zid: 9, pid: 3545 });
-    </script>
-    </div>
-    <!-- END ADREACTOR CODE -->
+        <center>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Top -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:728px;height:90px"
+             data-ad-client="ca-pub-9743974578479731"
+             data-ad-slot="6859678607"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         </center>
 
-   
          <center> <a class="btn btn-md btn-primary" href="<?php echo $domain; ?>/redirect/?site=<?php echo base64_encode($go); ?>" role="button">Visit Link &raquo;</a></center>
-         
-    <center><!-- BEGIN ADREACTOR CODE -->
-    <div id="avp_zid_6">
-    <script>
-    _avp.push({ tagid: 'avp_zid_6', alias: '/', type: 'banner', zid: 6, pid: 3545 });
-    </script>
-    </div>
-    <!-- END ADREACTOR CODE -->
-    </center>
+        
+        <center>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Top -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:728px;height:90px"
+             data-ad-client="ca-pub-9743974578479731"
+             data-ad-slot="6859678607"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        </center>
+        <h3>Random Post</h3>
         <?php
-echo '<h2 class="bmenu1"><align="center"> Top Chat iTunes </h2><div class="list2">';
-
-function tmgrab($url){$ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-curl_setopt($ch,CURLOPT_ENCODING,'gzip');
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, '1');
-curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-$header[] = "Accept-Language: en";
-$header[] = "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.0; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3
-";
-$header[] = "Pragma: no-cache";
-$header[] = "Cache-Control: no-cache";
-$header[] = "Accept-Encoding: gzip,deflate";
-$header[] = "Content-Encoding: gzip";
-$header[] = "Content-Encoding: deflate";
-curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-$load = curl_exec($ch);
-curl_close($ch);
-return $load;}
-
-$grab = strstr(tmgrab('https://www.apple.com/id/itunes/charts/songs/'),'class="section chart-grid');
-$tfkid = explode('</strong>',$grab);
-for($i=1; $i<=11; $i++){
-$file = cut($tfkid[$i],'l=en">','</a>');
-$artis = cut($tfkid[$i],'&amp;l=en">','</a></h4>');
-$imgz = cut($tfkid[$i],'src="','"');
-$img = explode('">',$file); 
-$img = str_replace('width="100" height="100"','width="60" height="60"',$file);
-$bajingan = explode('">',$file);
-$judul = cut($bajingan[0].'">','alt="','">');
-$link=$artis.'-'.$judul;
-$link=str_replace(' ', '-', $link);
-$link=strtolower($link);
-if(!empty($img[0])){
-echo '<table width="100%" class="otable"><tbody><tr><td width="60px"><img src="https://www.apple.com/'.$imgz.'" title="'.$artis.' - '.$judul.'" alt="'.$artis.' - '.$judul.'" width="60" height="60"></td><td><a title="'.$artis.' - '.$judul.'" alt="'.$artis.' - '.$judul.'" rel="dofollow" href="http://www.mpti.ga/video/'.$link.'"><b>'.$artis.' - '.$judul.'</b></a></td></tr></tbody></table>';
-}}
-
-function cut($content,$start,$end){
-if($content && $start && $end) {
-$r = explode($start, $content);
-if (isset($r[1])){
-$r = explode($end, $r[1]);
-return $r[0];
-}return '';}}
-?>
+        $xml=simplexml_load_file($feed);
+        $xmlink=$xml->channel->item[0]->link;
+        echo "<b><i class=\"glyphicon glyphicon-send\"></i> <a href=" .$xmlink. ">" .$xml->channel->item[0]->title ."</a></b><br/>";
+        echo "<b><i class=\"glyphicon glyphicon-send\"></i> <a href=" .$xmlink. ">" .$xml->channel->item[1]->title ."</a></b><br/>";
+        echo "<b><i class=\"glyphicon glyphicon-send\"></i> <a href=" .$xmlink. ">" .$xml->channel->item[2]->title ."</a></b><br/>";
+        echo "<b><i class=\"glyphicon glyphicon-send\"></i> <a href=" .$xmlink. ">" .$xml->channel->item[3]->title ."</a></b><br/>";
+        echo "<b><i class=\"glyphicon glyphicon-send\"></i> <a href=" .$xmlink. ">" .$xml->channel->item[4]->title ."</a></b><br/>";
+        ?>
       </div>
 
     </div> <!-- /container -->
@@ -155,3 +130,5 @@ return $r[0];
     <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
